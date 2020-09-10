d85327e55161a3cf56bb1315278c24296269549d
zed1025 <amit251098@yahoo.in>
2020-09-10 21:59:50 +0530

    created file for secrets

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,7 @@
 
 import os
 from pathlib import Path
+from mysite import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fwu=#_vs$d#2$2*c4y4-()yhq(d)#svf7&j8xyujty!e*)9+8@'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
